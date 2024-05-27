5688bd93789a1e1107e4484104ab4a9b6b28ef0d
birajkumarrpatel <birajrp45@gmail.com>
2024-05-27 16:10:39 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import ChallengeList from './components/ChallengeList';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import AddChallenge from './components/AddChallenge';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  const [challenges, setChallenges] = useState([]);
+
+  useEffect(() => {
+    fetchChallenges();
+  }, []); 
+
+  const fetchChallenges = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/challenges');
+      setChallenges(response.data);
+    } catch (error) {
+      console.error("Error: Fetching Challenges", error);
+    }
+  };
+
+  const handleChallengeAdded = () => {
+    fetchChallenges();
+  }
+
+  return (
+    <div className='container mt-5'>
+      <h1 className='text-center mb-4'>Monthly Challenges</h1>
+      <AddChallenge onChallengeAdded={handleChallengeAdded}/>
+      <ChallengeList challenges={challenges}/>
+    </div>
+  );
+}
+
+export default App;
